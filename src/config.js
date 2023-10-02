96cfde65de0f7678a88d108bbd1cd733877ab622
harsh <harsh.chouhan010@gmail.com>
2023-10-01 17:42:50 -0700

    Harsh commit with api url correction

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiKey : "https://bet-bitcoin.ai/api/",//process.env.REACT_APP_API_KEY,
+    apiKey : "https://api.bet-bitcoin.ai/api/",//process.env.REACT_APP_API_KEY,
     baseUrl:'/',
 
 }
